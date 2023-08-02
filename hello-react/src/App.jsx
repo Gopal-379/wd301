83cd5dd17c32fb872c24f945ab89d9ed98a13841
gopal <gopalpeddi379@gmail.com>
2023-08-02 16:43:14 +0530

    added tailwind to react-app

--- hello-react/src/App.jsx
+++ hello-react/src/App.jsx
@@ -1,11 +1,21 @@
+import TaskCard from "./TaskCard"
 
 function App() {
 
-  return (
-    <>
-      <h1>Vite + React</h1>
-    </>
-  )
+	return (
+		<div>
+			<div>
+				<h1>Pending</h1>
+				<TaskCard title="Build a website with static content"/>
+				<TaskCard title="Add a Blog"/>
+			</div>
+			<div>
+				<h1>Done</h1>
+				<TaskCard title="Design a Mockup"/>
+				<TaskCard title="Get approval from principal"/>
+			</div>
+		</div>
+	);
 }
 
 export default App
