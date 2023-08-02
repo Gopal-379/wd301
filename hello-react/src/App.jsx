d477759da6d56e38b3a5bcf8ac8273c7b2cab769
gopal <gopalpeddi379@gmail.com>
2023-08-02 17:45:12 +0530

    added required functionality for submission

--- hello-react/src/App.jsx
+++ hello-react/src/App.jsx
@@ -1,21 +1,54 @@
 import TaskCard from "./TaskCard"
 
-function App() {
 
-	return (
-		<div>
-			<div>
-				<h1>Pending</h1>
-				<TaskCard title="Build a website with static content"/>
-				<TaskCard title="Add a Blog"/>
-			</div>
-			<div>
-				<h1>Done</h1>
-				<TaskCard title="Design a Mockup"/>
-				<TaskCard title="Get approval from principal"/>
-			</div>
-		</div>
-	);
+function PendingTasks() {
+  return (
+    <div>
+      <h1 className="text-xl font-bold mb-4">Pending</h1>
+		<TaskCard
+			title="Build a website with static content"
+			dueDate="10th April"
+			completedAtDate={null}
+			assigneeName="Rohit S"
+		/>
+		<TaskCard
+			title="Add a Blog"
+			dueDate="22nd March"
+			completedAtDate={null}
+			assigneeName="Rohit M"
+		/>
+    </div>
+  );
 }
+
+function DoneTasks() {
+  return (
+    <div>
+      <h1 className="text-xl font-bold mb-4">Done</h1>
+        <TaskCard
+			title="Design a mockup"
+			dueDate={null}
+			completedAtDate="10th April"
+			assigneeName="Rohit M"
+		/>
+        <TaskCard
+			title="Get approval from principal"
+			dueDate={null}
+			completedAtDate="20th April"
+			assigneeName="Ajay S"
+		/>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="flex justify-center space-x-8 p-8">
+      <PendingTasks />
+      <DoneTasks />
+    </div>
+  );
+}
+
 
 export default App
--- hello-react/src/TaskCard.jsx
+++ hello-react/src/TaskCard.jsx
@@ -2,14 +2,23 @@
 import './TaskCard.css'
 
 const TaskCard = (props) => {
-  console.log(props)
-  return (
-    <div className='TaskItem'>
-      <h2 className="text-xl font-bold">{props.title}</h2>
-      <p>Completed on: due date...</p>
-      <p>Assignee: name...</p>
-    </div>
-  )
+    const isCompleted = props.completedAtDate !== null;
+    return (
+        <div className="bg-white rounded-lg p-4 shadow-md mb-4">
+            <h2 className="text-lg font-semibold mb-2">{props.title}</h2>
+            {isCompleted ? (
+                <>
+                    <p className="text-gray-500">Completed on: {props.completedAtDate}</p>
+                    <p className="text-gray-500">Assignee: {props.assigneeName}</p>
+                </>
+            ) : (
+                <>
+                    <p className="text-gray-500">Due on: {props.dueDate}</p>
+                    <p className="text-gray-500">Assignee: {props.assigneeName}</p>
+                </>
+            )}
+        </div>
+    );
 }
 
 export default TaskCard
