af2065c11f093cb9739745aab2508360acab6912
gopal <gopalpeddi379@gmail.com>
2023-09-06 16:25:59 +0530

    added notfound page navigation

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -1,32 +1,54 @@
 import {
   createBrowserRouter,
+  Navigate,
   RouterProvider,
 } from "react-router-dom";
 import HomePage from './pages/HomePage';
 import TaskListPage from './pages/TaskListPage';
 import TaskDetailsPage from './pages/TaskDetailsPage';
+import Signin from './pages/Signin';
+import ProtectedRoute from "./ProtectedRoute";
+import NotFound from "./pages/NotFound";
 import Layout from './Layout';
 
 const router = createBrowserRouter([
-  {
-    element: (
-      <Layout />
-    ),
-    children: [
-      {
-        path: "/",
-        element: (<HomePage />)
-      },
-      {
-        path: "tasks",
-        element: (<TaskListPage />)
-      },
-      {
-        path: "tasks/:id",
-        element: (<TaskDetailsPage />)
-      },
-    ]
-  }
+	{
+		path: "/",
+		element: <Navigate to="/signin" replace />,
+	},
+	{
+		path: "/signin",
+		element: <Signin />,
+	},
+	{
+		path: "/notfound",
+		element: <NotFound />,
+	},
+	{
+		element: (
+		<ProtectedRoute>
+			<Layout />
+		</ProtectedRoute>
+		),
+		children: [
+		{
+			path: "home",
+			element: <HomePage />,
+		},
+		{
+			path: "tasks",
+			element: <TaskListPage />,
+		},
+		{
+			path: "tasks/:id",
+			element: <TaskDetailsPage />,
+		},
+		{
+			path: "*",
+			element: <Navigate to="/notfound" replace />
+		},
+		],
+	},
 ]);
 
 const App = () => {
---
+++ smarter-tasks/src/pages/NotFound.tsx
@@ -0,0 +1,24 @@
+import { useNavigate } from "react-router-dom";
+
+const NotFound = () => {
+    const redirect = useNavigate();
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-100">
+            <div className="bg-white p-8 rounded shadow-lg">
+                <h1 className="text-4xl text-red-500 font-bold mb-2">404</h1>
+                <p className="text-lg text-gray-700 mb-4">Page Not Found</p>
+                <p className="text-gray-600 mb-4">
+                    Sorry, the page you are looking for does not exist.
+                </p>
+                <button
+                    onClick={() => redirect('/')}
+                    className="text-blue-500 hover:underline focus:outline-none focus:ring focus:ring-blue-300 transition"
+                >
+                    Go back to the homepage
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default NotFound;
