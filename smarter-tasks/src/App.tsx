5e057cfee99f1b81cb61fcd20994c44bf70bec86
gopal <gopalpeddi379@gmail.com>
2023-09-26 23:46:20 +0530

    changed everything to application state modelling

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -1,114 +1,18 @@
-// import {
-//   createBrowserRouter,
-//   Navigate,
-//   RouterProvider,
-// } from "react-router-dom";
-// import HomePage from './pages/HomePage';
-// import TaskListPage from './pages/TaskListPage';
-// import TaskDetailsPage from './pages/TaskDetailsPage';
-// import ProtectedRoute from "./ProtectedRoute";
-// import Signin from './pages/Signin';
-// import Notfound from "./pages/Notfound";
-// import Signup from "./pages/signup";
-// import Layout from './Layout';
-
-// const router = createBrowserRouter([
-// 	{
-// 		path: "/",
-// 		element: <Signup />,
-// 	},
-// 	{
-// 		path: "/signup",
-// 		element: <Signup />,
-// 	},
-// 	{
-// 		path: "/signin", 
-// 		element: <Signin />,
-// 	},
-// 	{
-// 		path: "/notfound",
-// 		element: <Notfound />,
-// 	},
-// 	{
-// 		element: (
-// 		<ProtectedRoute>
-// 			<Layout />
-// 		</ProtectedRoute>
-// 		),
-// 		children: [
-// 		{
-// 			path: "home",
-// 			element: <HomePage />,
-// 		},
-// 		{
-// 			path: "tasks",
-// 			element: <TaskListPage />,
-// 		},
-// 		{
-// 			path: "tasks/:id",
-// 			element: <TaskDetailsPage />,
-// 		},
-// 		{
-// 			path: "*",
-// 			element: <Navigate to="/notfound" replace />
-// 		},
-// 		],
-// 	},
-// ]);
-
-// const App = () => {
-// 	return (
-// 		<RouterProvider router={router} />
-// 	);
-// }
-
-import {
-	createBrowserRouter,
-	RouterProvider,
-} from "react-router-dom";
-import Notfound from "./pages/Notfound";
-import Signup from './pages/signup';
-import Signin from './pages/signin';
-import Dashboard from "./pages/dashboard";
-import ProtectedRoute from "./ProtectedRoute";
-
-
-const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <Signup />,
-	},
-	{
-		path: "/signup",
-		element: <Signup />,
-	},
-	{
-		path: "/signin", // then we've added the signin route
-		element: <Signin />,
-	},
-	{
-		path: "/notfound",
-		element: <Notfound />,
-	},
-	{
-		path: "/dashboard",
-		element: (
-			<ProtectedRoute>
-				<Dashboard />
-			</ProtectedRoute>
-		),
-	},
-	{
-		path: "*",
-		element: <Notfound />,
-	}
-]);
+import { useContext } from "react"; 
+import { RouterProvider } from "react-router-dom";
+import router from "./routes";
+import { ThemeContext } from "./context/theme";
+import { ProjectsProvider } from "./context/projects/context";
 
 const App = () => {
-  return (
-    <RouterProvider router={router} />
-  );
+	const { theme } = useContext(ThemeContext);
+	return (
+		<div className={`h-screen w-full mx-auto py-2 ${theme === "dark" ? "dark" : ""}`}>
+			<ProjectsProvider>
+				<RouterProvider router={router} />
+			</ProjectsProvider>
+		</div>
+	);
 }
-
 
 export default App;
